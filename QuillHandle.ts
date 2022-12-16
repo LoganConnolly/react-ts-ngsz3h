8bb649bf50c97e715a954042d86cab03029c0d7b
LoganConnolly <77025699+LoganConnolly@users.noreply.github.com>
2022-12-16 13:09:43 -0800

    bruh

--- QuillHandle.ts
+++ QuillHandle.ts
@@ -2,10 +2,11 @@
 class QuillHandle {
 	to_decode: string = '';
 	_equation: string = '';
+	eq_numbs: number[] = [];
 	readonly errors = {
 		no_eq: 'Equation equals null!',
 		no_frac: 'DecodeFrac was called but there is no fraction!',
-		no_num: 'There was no number in the equation!',
+		no_num: 'There was no number in the equation!'
 	};
 
 	constructor(equation: string) {
@@ -17,58 +18,19 @@ class QuillHandle {
 		this.to_decode = equation;
 	}
 	UpdateDecode = (e: string) => {
+		this.eq_numbs = [];
 		this.to_decode = e;
+		let numbs = e.split(/^\d/);
+		for (let x of numbs){
+			this.eq_numbs.push(Number(x));
+		}
 	};
 
 	DecodeQuill = () => {
 		let decoded: string = this.to_decode;
 		decoded = decoded.replace(/\\cdot/g, '*');
 		let operations: string[] = [];
-		if (!decoded.includes('\\frac')) {
-			operations = decoded.split('\\');
-		} else {
-			//All of this to decode one fraction -_-
-			let frac = this.DecodeFrac(decoded);
-			let l_frac = frac.length;
-			let numbs: number[] = [];
-			let count: number = 0;
-			for (let i = 0; i < l_frac; i++) {
-				let current: number = NaN;
-				let temp = frac[i].match(/\d/g);
-				let s_temp = frac[i].match(/[+\-*\/]/g);
-				if (temp !== null) {
-					for (let x of temp) {
-						
-					}
-					current = numbs[0];
-					if (s_temp !== null){
-						for (let y of s_temp) {
-							let n_two = numbs[count + 1];
-							switch (y) {
-								case '*':
-									current *= n_two;
-									break;
-								case '+':
-									current += n_two;
-									break;
-								case '-':
-									current -= n_two;
-									break;
-								case '/':
-									current /= n_two;
-									break;
-								default:
-									throw `Something went very wrong (${this.errors.no_num})`;
-							}
-							count++;
-						}
-					}
-				}
-			}
-		}
-
-		for (let x of operations) {
-		}
+		
 	};
 	//Returns the numerator and denominator of the fraction.
 	DecodeFrac = (decoded: string) => {
